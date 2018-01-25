20562d91cdbebf59fed4ddfa4ab68a28d63878de
Renato Rodrigues de Araujo <renato.r.araujo@gmail.com>
2018-01-24 22:34:01 -0200

    Added todo for TDD ApiController functional testing

--- tests/Functional/ApiControllerTest.php
+++ tests/Functional/ApiControllerTest.php
@@ -4,8 +4,17 @@ namespace TicTacToe\Tests\Functional;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * Class ApiControllerTest
+ * @package TicTacToe\Tests\Functional
+ *
+ * @author Renato Rodrigues de Araujo <renato.r.araujo@gmail.com>
+ */
 class ApiControllerTest extends WebTestCase
 {
+    /**
+     * @todo Implement the request body and validate the response
+     */
     public function testMakeMove(): void
     {
         $client = static::createClient();
@@ -19,18 +28,30 @@ class ApiControllerTest extends WebTestCase
         );
     }
 
+    /**
+     * @todo
+     */
     public function testMakeMoveBotWins(): void
     {
     }
 
+    /**
+     * @todo
+     */
     public function testInvalidMakeMoveRequest(): void
     {
     }
 
+    /**
+     * @todo
+     */
     public function testMakeMovePlayerWins(): void
     {
     }
 
+    /**
+     * @todo
+     */
     public function testMakeMoveTiedGame(): void
     {
     }
